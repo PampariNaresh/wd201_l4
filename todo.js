4c51e6b0b585a80ca1f75c3ca55deb0be741e168
PampariNaresh <pamparinaresh2001@gmail.com>
2023-01-07 09:44:04 +0530

    first commit

---
+++ todo.js
@@ -0,0 +1,33 @@
+const todoList = () => {
+    all = []
+   const add = (todoItem) => {
+     all.push(todoItem)
+   }
+   const markAsComplete = (index) => {
+     all[index].completed = true
+   }
+ 
+   const overdue = () => {
+     return all.filter((todos) => 
+       todos.dueDate == yesterday
+     )
+   }
+ 
+   const dueToday = () => {
+     return all.filter((todos) =>
+       todos.dueDate == today
+     )
+   }
+ 
+   const dueLater = () => {
+     return all.filter((todos) =>
+       todos.dueDate == tomorrow)
+   }
+ 
+   function toDisplayableList(list) {
+       return list.map(todos => `${todos.completed ? `[x]` : `[ ]`} ${todos.title} ${todos.dueDate != today ?todos.dueDate:" "}`).join('\n')
+   }
+   return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
+ }
+ 
+ module.exports =todoList;
